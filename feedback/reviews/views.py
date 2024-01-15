1902629b2eb01b94a745c64678d789b6b1b4570a
SztGellert <sztankgellert@gmail.com>
2024-01-16 00:41:12 +0100

    use form validations

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -7,11 +7,13 @@ from .forms import ReviewForm
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
+
         if form.is_valid():
             print(form.cleaned_data)
             return HttpResponseRedirect("/thank-you")
+    else:
+        form = ReviewForm()
         
-    form = ReviewForm()
     return render(request, "reviews/review.html", {"form": form})
 
 
