e4875174e9bc976e158fc6bbe90b860997014491
SztGellert <sztankgellert@gmail.com>
2024-01-16 03:21:31 +0100

    use TemplateView

--- feedback/reviews/urls.py
+++ feedback/reviews/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+
 urlpatterns = [
     path("", views.ReviewView.as_view()),
-    path("thank-you", views.thank_you)
+    path("thank-you", views.ThankYouView.as_view()),
+    path("reviews", views.ReviewListView.as_view()),
 ]
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -1,6 +1,8 @@
+from typing import Any
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.views import View
+from django.views.generic.base import TemplateView
 
 from .forms import ReviewForm
 from .models import Review
@@ -19,6 +21,25 @@ class ReviewView(View):
             return HttpResponseRedirect("/thank-you")
 
         return render(request, "reviews/review.html", {"form": form})
+
+
+class ThankYouView(TemplateView):
+    template_name = "reviews/thank_you.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context["message"] = "This works!"
+        return context
+
+
+class ReviewListView(TemplateView):
+    template_name = "reviews/review_list.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        reviews = Review.objects.all()
+        context["reviews"] = reviews
+        return context
 
 
 def review(request):
