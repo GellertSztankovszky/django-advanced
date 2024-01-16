571fdbf1115170d8565a7e0350e70fedbf06394b
SztGellert <sztankgellert@gmail.com>
2024-01-16 02:40:01 +0100

    use ModelForms

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -1,6 +1,7 @@
 from django import forms
+from .models import Review
 
-
+"""
 class ReviewForm(forms.Form):
     user_name = forms.CharField(
         label="Your Name",
@@ -11,3 +12,23 @@ class ReviewForm(forms.Form):
         })
     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+"""
+
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = "__all__"
+        labels = (
+            {
+                "user_name": "Your name",
+                "review_text": "Your Feedback",
+                "rating": "Your Rating",
+            },
+        )
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+                "max_length": "Please enter a shorter name!",
+            }
+        }
--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -10,13 +10,9 @@ def review(request):
         form = ReviewForm(request.POST)
 
         if form.is_valid():
-            review = Review(
-                user_name=form.cleaned_data["user_name"],
-                review_text=form.cleaned_data["review_text"],
-                rating=form.cleaned_data["rating"],
-            )
-            review.save()
+            form.save()
             return HttpResponseRedirect("/thank-you")
+        
     else:
         form = ReviewForm()
     return render(request, "reviews/review.html", {"form": form})
