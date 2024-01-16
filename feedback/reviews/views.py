25cc1298ee09090fa9a93302533fd56aa6c454ac
SztGellert <sztankgellert@gmail.com>
2024-01-16 03:30:40 +0100

    added Detail Template

--- feedback/reviews/views.py
+++ feedback/reviews/views.py
@@ -42,6 +42,17 @@ class ReviewListView(TemplateView):
         return context
 
 
+class SingleReviewView(TemplateView):
+    template_name = "reviews/single_review.html"
+
+    def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        review_id = kwargs["id"]
+        selected_review = Review.objects.get(pk=review_id)
+        context["review"] = selected_review
+        return context
+
+
 def review(request):
     if request.method == "POST":
         form = ReviewForm(request.POST)
