52436e1fce8ec5222c76d6dc09eac2bc9aedd877
SztGellert <sztankgellert@gmail.com>
2024-01-16 02:29:11 +0100

    store form data in db

--- feedback/reviews/forms.py
+++ feedback/reviews/forms.py
@@ -4,7 +4,7 @@ from django import forms
 class ReviewForm(forms.Form):
     user_name = forms.CharField(
         label="Your Name",
-        max_length=200,
+        max_length=100,
         error_messages={
             "required": "Xour name must not be empty!",
             "max_length": "Please enter a shorter name!",
